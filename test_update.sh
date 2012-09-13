1f7c04ef7410062e250f1c901f0624028fd8f299
Chris Jones <jones.chris.g@gmail.com>
2012-09-13 17:14:01 -0500

    Change default update location to /sdcard, and make sure it's mounted.
    Signed-off-by: Marshall Culpepper <marshall.law@gmail.com>

--- test_update.sh
+++ test_update.sh
@@ -1,20 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
 if [[ ! -n "$B2G_HOME" ]]; then
   echo "Error: B2G_HOME must be set to run this script"
   exit 1
 fi
 
+(adb shell vdc volume list | grep '110 sdcard .* 4') > /dev/null || \
+  (echo "Error: /sdcard must be mounted"; exit 1)
+
 if [[ ! -f "boot.img" ]]; then
-  echo "Error: boot.img not found, please place it in the top level directory and try again"
-  exit 1
+  echo "Warning: boot.img not found, please place it in the top level directory and try again if you want a kernel update."
 fi
 
 KEYDIR=$B2G_HOME/build/target/product/security
+DOWNLOAD_DIR=/sdcard
 
 rm update*.zip
 zip -r update.zip META-INF system boot.img
 java -Xmx2048m -jar signapk.jar -w $KEYDIR/testkey.x509.pem $KEYDIR/testkey.pk8 update.zip update-signed.zip
-adb -d push update-signed.zip /cache/update.zip
-adb -d shell 'mkdir -p /cache/recovery; echo --update_package=/cache/update.zip > /cache/recovery/command'
+adb -d push update-signed.zip $DOWNLOAD_DIR/update.zip
+adb -d shell "mkdir -p /cache/recovery; echo --update_package=$DOWNLOAD_DIR/update.zip > /cache/recovery/command"
 adb -d reboot recovery
