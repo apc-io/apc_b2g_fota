9f3a55d4642d6b20dd73feb28ffb9f6140f18e7f
Nguyen Thanh Trung <nguyenthanh.trung@nomovok.vn>
2013-09-13 15:49:06 +0700

    added build.prop to OTA and auto generate some information needed for update.xml

--- create_ota.sh
+++ create_ota.sh
@@ -6,6 +6,7 @@ if [[ ! -n "$B2G_HOME" ]]; then
 fi
 
 SOURCE_BOOT_IMG=$B2G_HOME/out/target/product/wmid/boot.img
+SOURCE_BUILD_PROP=$B2G_HOME/out/target/product/wmid/system/build.prop
 KEYDIR=$B2G_HOME/build/target/product/security
 OTA_PREFIX=apc8950_b2g_ota
 OTA_SUFFIX=$(date +%Y%m%d.%H%M)
@@ -23,7 +24,31 @@ if [[ ! -f "boot.img" ]]; then
 fi
 
 rm -f update*.zip
-rm -f ${OTA_PREFIX}_*.zip
+rm -f ${OTA_PREFIX}_*.{zip,stat}
+
+rm -f system/build.prop
+mkdir -p system
+ln "$SOURCE_BUILD_PROP" system
+
 zip -r update.zip META-INF system boot.img
 java -Xmx2048m -jar signapk.jar -w $KEYDIR/testkey.x509.pem $KEYDIR/testkey.pk8 update.zip $OTA_ZIP
 
+# get needed info for update.xml
+echo "Generating archive file stats ..."
+HASH_FUNC="sha512"
+HASH_CMD="$HASH_FUNC"sum
+#echo "Generating $HASH_CMD for $OTA_ZIP ..."
+HASH_VALUE=$($HASH_CMD $OTA_ZIP)
+#echo "value is $HASH_VALUE"
+
+#echo "Getting file size of $OTA_ZIP ..."
+ARCHIVE_SIZE=$(stat -c%s $OTA_ZIP)
+#echo "value is $ARCHIVE_SIZE"
+
+STAT_FILE=${OTA_PREFIX}_${OTA_SUFFIX}.stat
+echo "File name: $OTA_ZIP" > $STAT_FILE
+echo "File size: $ARCHIVE_SIZE" >> $STAT_FILE
+echo "Hash function: ${HASH_FUNC^^}" >> $STAT_FILE
+echo "Hash value: ${HASH_VALUE}" >> $STAT_FILE
+
+echo "Done, look $STAT_FILE for the stats"
